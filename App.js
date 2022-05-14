5702f5e9e9ce71c093262a53fe87c39b546ee986
harukin-DeskMini <harukin@haruk.in>
2022-05-14 17:14:18 +0900

    レイアウトフィックスのテスト

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Entypo } from '@expo/vector-icons';
 //import { Button, BottomSheet, ListItem, Header } from 'react-native-elements';
@@ -63,96 +63,102 @@ export default function App() {
   );
 
   return (
-    <SafeAreaProvider style={{ backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center',width:"100%"}}>
-      <View style={{width:"100%",height:60,backgroundColor:"#00E8E3",flexDirection:"row"}}>
-        <View style={{flex:1}} />
-        <Text>てすと</Text>
-        <View style={{flex:1}} />
-        <IconButton aria-label="delete" size="large" onClick={()=>setDrawer(true)}>
-          <DehazeIcon fontSize="inherit" />
-        </IconButton>
+    <SafeAreaProvider style={{ backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center',width:"100%", height:"100%"}}>
+      <ScrollView style={{ width:"100%", height:300}} >
+        <View style={{width:"100%",height:60,backgroundColor:"#00E8E3",flexDirection:"row"}}>
+          <View style={{flex:1}} />
+          <Text>てすと</Text>
+          <View style={{flex:1}} />
+          <IconButton aria-label="delete" size="large" onClick={()=>setDrawer(true)}>
+            <DehazeIcon fontSize="inherit" />
+          </IconButton>
+        </View>
+        <Text>testApp</Text>
+        <View style={{width:"100%",height:500,backgroundColor:"black"}}>
+          <HoloButton>
+            てすと
+          </HoloButton>  
+          <HoloButton>
+            ほげほげ
+          </HoloButton>  
+          <Switch4 flag={testSwitch1} onPress={()=>setTestSwitch1(!testSwitch1)}>
+            てすとスイッチ
+          </Switch4>
+          <Switch44 flag={testSwitch2} onPress={()=>setTestSwitch2(!testSwitch2)}>
+            てすとスイッチ
+          </Switch44>
+        </View>
+        
+        <Card sx={{ minWidth: 275 }}>
+          <CardContent>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+              Word of the Day
+            </Typography>
+            <Typography variant="h5" component="div">
+              test
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              adjective
+            </Typography>
+            <Typography variant="body2">
+              well meaning and kindly.
+              <br />
+              {'"a benevolent smile"'}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+          <View style={{flex:1}} />
+        <Button variant="outlined" onClick={()=>setIsVisible(true)}>
+          Open alert dialog
+        </Button>
+        <Image style={{height: 200,width: 200}} source={require("./assets/Harukin-main.png")} />{/* 
+        <BottomSheet
+          isVisible={isVisible}
+          containerStyle={{ backgroundColor: 'rgba(0.5, 0.25, 0, 0.2)' }}
+        >
+          {list.map((l, i) => (
+            <ListItem key={i} containerStyle={l.containerStyle} onPress={l.onPress}>
+              <ListItem.Content>
+                <ListItem.Title style={l.titleStyle}>{l.title}</ListItem.Title>
+              </ListItem.Content>
+            </ListItem>
+          ))}
+        </BottomSheet> */}
+        <Dialog
+          open={isVisible}
+          onClose={()=>setIsVisible(false)}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            ほげほげ
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              ふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふが
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={()=>setIsVisible(false)}>にゃーん</Button>
+            <Button onClick={()=>setIsVisible(false)} autoFocus>ほげふが</Button>
+          </DialogActions>
+        </Dialog>
+        <SwipeableDrawer
+          anchor={"right"}
+          open={drawer}
+          onClose={()=>setDrawer(false)}
+          onOpen={()=>setDrawer(true)}
+        >{list("right")}
+        </SwipeableDrawer>
+      </ScrollView>
+      <View style={{width:"100%",height:100}}>
+        <Text>ほげほげ</Text>
+        <Text>ほげほげ</Text>
+        <Text>ほげほげ</Text>
       </View>
-      <Text>testApp</Text>
-      <View style={{width:"100%",height:500,backgroundColor:"black"}}>
-        <HoloButton>
-          てすと
-        </HoloButton>  
-        <HoloButton>
-          ほげほげ
-        </HoloButton>  
-        <Switch4 flag={testSwitch1} onPress={()=>setTestSwitch1(!testSwitch1)}>
-          てすとスイッチ
-        </Switch4>
-        <Switch44 flag={testSwitch2} onPress={()=>setTestSwitch2(!testSwitch2)}>
-          てすとスイッチ
-        </Switch44>
-      </View>
-      
-      <Card sx={{ minWidth: 275 }}>
-        <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Word of the Day
-          </Typography>
-          <Typography variant="h5" component="div">
-            test
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            adjective
-          </Typography>
-          <Typography variant="body2">
-            well meaning and kindly.
-            <br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
-        <View style={{flex:1}} />
-      <Button variant="outlined" onClick={()=>setIsVisible(true)}>
-        Open alert dialog
-      </Button>
-      <Image style={{height: 200,width: 200}} source={require("./assets/Harukin-main.png")} />{/* 
-      <BottomSheet
-        isVisible={isVisible}
-        containerStyle={{ backgroundColor: 'rgba(0.5, 0.25, 0, 0.2)' }}
-      >
-        {list.map((l, i) => (
-          <ListItem key={i} containerStyle={l.containerStyle} onPress={l.onPress}>
-            <ListItem.Content>
-              <ListItem.Title style={l.titleStyle}>{l.title}</ListItem.Title>
-            </ListItem.Content>
-          </ListItem>
-        ))}
-      </BottomSheet> */}
-      <Dialog
-        open={isVisible}
-        onClose={()=>setIsVisible(false)}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          ほげほげ
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            ふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふがふが
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={()=>setIsVisible(false)}>にゃーん</Button>
-          <Button onClick={()=>setIsVisible(false)} autoFocus>ほげふが</Button>
-        </DialogActions>
-      </Dialog>
-      <SwipeableDrawer
-        anchor={"right"}
-        open={drawer}
-        onClose={()=>setDrawer(false)}
-        onOpen={()=>setDrawer(true)}
-      >{list("right")}
-      </SwipeableDrawer>
-      <StatusBar style="auto" />
     </SafeAreaProvider>
   );
 }
